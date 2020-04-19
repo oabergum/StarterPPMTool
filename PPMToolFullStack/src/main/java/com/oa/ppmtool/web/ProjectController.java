04a251740213aa40aca6e1437d02b2f93ed9e224
Odd Arne Bergum <oab@forskningsradet.no>
2020-04-19 16:50:34 +0200

    Branch_5: Returning Map of errors if any occurs

--- PPMToolFullStack/src/main/java/com/oa/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/oa/ppmtool/web/ProjectController.java
@@ -1,11 +1,15 @@
 package com.oa.ppmtool.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +27,13 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
+
         if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
         Project savedProject = projectService.saveOrUpdateProject(project);
