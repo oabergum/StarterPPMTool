2321c6b513e6619d30e8013e858230b04f279cd1
Odd Arne Bergum <oab@forskningsradet.no>
2020-04-19 17:15:04 +0200

    Branch_6: Added validation in separate class to be able to reuse code for other services

--- PPMToolFullStack/src/main/java/com/oa/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/oa/ppmtool/web/ProjectController.java
@@ -1,21 +1,18 @@
 package com.oa.ppmtool.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oa.ppmtool.domain.Project;
+import com.oa.ppmtool.services.MapValidationErrorService;
 import com.oa.ppmtool.services.ProjectService;
 
 @RestController
@@ -25,15 +22,15 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error : result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = mapValidationErrorService.validateRequest(result);
+        if (errorMap != null) {
+            return errorMap;
         }
 
         Project savedProject = projectService.saveOrUpdateProject(project);
