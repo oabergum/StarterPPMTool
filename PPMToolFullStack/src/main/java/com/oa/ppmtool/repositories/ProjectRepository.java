b63c8f9de1c123bd1c267f2e3dde4ece28500ede
Odd Arne Bergum <oab@forskningsradet.no>
2020-05-03 16:42:56 +0200

    Branch_8: Implemented findProjectByIdentifier

--- PPMToolFullStack/src/main/java/com/oa/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/com/oa/ppmtool/repositories/ProjectRepository.java
@@ -8,6 +8,7 @@ import com.oa.ppmtool.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+//    @Override
+//    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Project findByProjectIdentifier(String projectIdentifier);
 }
